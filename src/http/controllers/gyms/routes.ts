71bd3f49ae1c7acc5217d5fd59597e3f6d4f8e9c
Felipe DR <felipe.corp7@gmail.com>
2025-07-09 18:43:30 -0300

    feat: class-06 other academy controllers

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,14 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
+import { create } from './create'
+import { nearby } from './nearyby'
+import { search } from './search'
+
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
+
+  app.post('/gyms', create)
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
 }
